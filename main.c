670256e39097b945bf5b700175b85f3607164ab4
ske <contact@ske.land>
2025-12-05 21:02:21 -0300

    lists sort of

--- main.c
+++ main.c
@@ -46,7 +46,7 @@ int main()
   const char *input =
     "(repeat 10 (do\n"
       "(print 'hi')\n"
-      "(print (add 1 2 3 4 5))\n"
+      "(print [add 1 2 3 4 5])\n"
     "))";
   while ((lskipspace(&input), *input))
     lexec(&input, command, false);
--- tclisp.h
+++ tclisp.h
@@ -15,35 +15,28 @@ static void lskipspace(const char **src) {
 
 static char *lexec(const char **src, char *(*sexec)(const char **src), bool raw) {
   const char *start = (lskipspace(src), *src);
-  if (**src == 0) {
-    lerr("unexpected eof");
-  } else if (**src == '(') {
-    if (raw) {
-      const char *start = (*src)++;
-      while (**src && **src != ')') (lexec(src, sexec, raw), lskipspace(src));
-      if (!*src) lerr("mismatched (");
-      const char *end = ++(*src);
-      return strncpy((char*)calloc(1, end-start+1), start, end-start);
+  if (**src == '(' || **src == '[') {
+    if ((*src)++ && (raw || *start == '[')) {
+      while (**src && **src != (*start == '(' ? ')' : ']')) (lexec(src, sexec, raw), lskipspace(src));
+      if (!*(*src)++) lerr("mismatched bracket");
+      return strncpy((char*)calloc(1, *src-start+1), start, *src-start);
     }
-    return sexec(((*src)++, src));
-  } else if (**src == '\'') {
-    const char *start = ++(*src);
+    return sexec(src);
+  } else if (**src == '\'' && (*src)++) {
     while (**src && **src != '\'') (*src)++;
-    const char *end = *src;
     if (!*(*src)++) lerr("unclosed string");
-    return strncpy((char*)calloc(1, end-start+1), start, end-start);
-  } else if (**src == ')') lerr("stray )");
+    return strncpy((char*)calloc(1, (*src-1)-start+1), start+1, (*src-1)-(start+1));
+  } else if (**src == ')' || **src == ']') lerr("stray bracket");
+  else if (**src == 0) lerr("unexpected eof");
 
-  while (**src && **src != '\'' && **src != '(' && **src != ')' && !isspace(**src)) (*src)++;
+  while (**src && **src != '\'' && **src != '(' && **src != ')' && **src != '[' && **src != ']' && !isspace(**src)) (*src)++;
   return strncpy((char*)calloc(1, (*src)-start+1), start, (*src)-start);
 }
 
 static char *lparam(const char **src, char *(*sexec)(const char **src), bool raw) {
-  if (lskipspace(src), **src == ')') return NULL;
-  return lexec(src, sexec, raw);
+  return (lskipspace(src), **src == ')') ? NULL : lexec(src, sexec, raw);
 }
 
 static void lendparam(const char **src) {
-  if (lskipspace(src), **src != ')') lerr("expected end of parameters");
-  (*src)++;
+  return (lskipspace(src), **src != ')') ? lerr("expected end of parameters") : (void)(*src)++;
 }
